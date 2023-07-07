a79ea583c3dff4ecefc6c0cbd8313411827e8f08
MollyHe5 <molly.cn.mail@gmail.com>
2023-07-07 01:29:30 -0400

    fix tests

--- tests/ZeroValueTest.sol
+++ tests/ZeroValueTest.sol
@@ -8,16 +8,13 @@ contract ZeroValueDemo {
 
     struct Param {
         uint256 a;
-        uint256 c;
         bool b;
     }
 
     Param p;
 
-    function setP(uint256 a, bool b) public {
-        p.a = a;
-        p.c = a;
-        p.b = b;
+    function setP(Param calldata param) public {
+        p = param;
     }
 
     function setA(uint256 a) public {
@@ -35,54 +32,60 @@ contract ZeroValueTest is GasMeasure {
 
     function test1() public {
 
+        ZeroValueDemo.Param memory param = ZeroValueDemo.Param(1,true);
+
         uint256 startGas = gasleft();
-        demo.setP(1, true);
+        demo.setP(param);
         uint256 endGas = gasleft();
         uint256 gasUsage1 = startGas - endGas;
-        emit log_named_uint("gasUsage1_1", gasUsage1); // 72373 gas
+        emit log_named_uint("gasUsage1_1", gasUsage1); // 51049 gas
 
+        param = ZeroValueDemo.Param(2,true);
         startGas = gasleft();
-        demo.setP(2, true);
+        demo.setP(param);
         endGas = gasleft();
         gasUsage1 = startGas - endGas;
-        emit log_named_uint("gasUsage1_2", gasUsage1); // 1958 gas
+        emit log_named_uint("gasUsage1_2", gasUsage1); // 2740 gas
 
+        param = ZeroValueDemo.Param(0,false);
         startGas = gasleft();
-        demo.setP(0, false);
+        demo.setP(param);
         endGas = gasleft();
         gasUsage1 = startGas - endGas;
-        emit log_named_uint("gasUsage1_3", gasUsage1); // 1958 gas
+        emit log_named_uint("gasUsage1_3", gasUsage1); // 2740 gas
 
+        param = ZeroValueDemo.Param(2,true);
         startGas = gasleft();
-        demo.setP(2, true);
+        demo.setP(param);
         endGas = gasleft();
         gasUsage1 = startGas - endGas;
-        emit log_named_uint("gasUsage1_4", gasUsage1); // 61658 gas
+        emit log_named_uint("gasUsage1_4", gasUsage1); // 42540 gas
 
+        param = ZeroValueDemo.Param(1,true);
         startGas = gasleft();
-        demo.setP(1, true);
+        demo.setP(param);
         endGas = gasleft();
         gasUsage1 = startGas - endGas;
-        emit log_named_uint("gasUsage1_5", gasUsage1); // 1958 gas
+        emit log_named_uint("gasUsage1_5", gasUsage1); // 2740 gas
 
 
         startGas = gasleft();
         demo.setA(2);
         endGas = gasleft();
         uint256 gasUsage2 = startGas - endGas;
-        emit log_named_uint("gasUsage2_1", gasUsage2); // 1302 gas
+        emit log_named_uint("gasUsage2_1", gasUsage2); // 1280 gas
 
         startGas = gasleft();
         demo.setA(1);
         endGas = gasleft();
         gasUsage2 = startGas - endGas;
-        emit log_named_uint("gasUsage2_2", gasUsage2); // 1302 gas
+        emit log_named_uint("gasUsage2_2", gasUsage2); // 1280 gas
 
         startGas = gasleft();
         demo.setA(2);
         endGas = gasleft();
         gasUsage2 = startGas - endGas;
-        emit log_named_uint("gasUsage2_3", gasUsage2); // 1302 gas
+        emit log_named_uint("gasUsage2_3", gasUsage2); // 1280 gas
 
 
         startGas = gasleft();
